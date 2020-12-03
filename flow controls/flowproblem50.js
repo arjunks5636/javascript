4166e0caeb7b771716494d1e64669c198894b6d9
Arjun K S <anju56365636@gmail.com>
2020-12-03 12:45:04 +0530

    file renamed and problem done

---
+++ flow controls/flowproblem50.js
@@ -0,0 +1,10 @@
+var num1=10;
+var num2=4;
+var num3;
+num3=num1+num2;
+if(num3===50){
+    console.log("the sum of given numbers is 50");
+}
+else{
+    console.log("the sum of given numbers is not 50");
+}
